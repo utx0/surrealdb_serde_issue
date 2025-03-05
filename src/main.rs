25aee821ac79cf79c3fc917af1b16cd063e79fa1
utx0_ <utx0@mettadata.com>
2025-03-05 15:35:41 +1100

    Added simple timers

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use serde_json::from_value;
 use solana_transaction_status::UiConfirmedBlock;
 use std::fs::File;
 use std::io::BufReader;
+use std::time::Instant;
 use surrealdb::Surreal;
 use surrealdb::engine::remote::ws::Ws;
 use surrealdb::opt::auth::Root;
@@ -23,28 +24,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let reader = BufReader::new(file);
         let json_obj: Value = serde_json::from_reader(reader).unwrap();
 
+        let start = Instant::now();
         // Save the json data into surrealdb
         let _json_data: Option<Value> = match db.create("json_data").content(json_obj.clone()).await
         {
             Ok(result) => result,
             Err(e) => {
-                println!("Error: {}", e);
+                // eprintln!("Error: {}", e);
                 None
             }
         };
+        let duration = start.elapsed();
+        println!("0000: Time elapsed: {:?}", duration);
 
         // Deserialize into a block type
         let block_data: UiConfirmedBlock = from_value(json_obj).unwrap();
 
+        let start = Instant::now();
         // Save the block data into surrealdb
         let _block: Option<UiConfirmedBlock> =
             match db.create("block_data").content(block_data).await {
                 Ok(result) => result,
                 Err(e) => {
-                    println!("Error: {}", e);
+                    // eprintln!("Error: {}", e);
                     None
                 }
             };
+        let duration = start.elapsed();
+        println!("1111: Time elapsed: {:?}", duration);
     }
 
     Ok(())
